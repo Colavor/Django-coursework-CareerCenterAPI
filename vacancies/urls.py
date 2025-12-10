118025fccc1ef6f29e2aa56adeb74e8cc7514eff
Grigoryev Egor <egorgrigoryeff@gmail.com>
2025-12-10 19:17:01 +0300

    написал url внутри приложения

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    VacancyViewSet, CompanyViewSet, StudentViewSet,
+    ResumeViewSet, ApplicationViewSet
+)
 
-urlpatterns = [
-    path('', views.vacancies, name='vacancies')
-]
+router = DefaultRouter()
+router.register('vacancies', VacancyViewSet, basename='vacancy') #имя-ярлык для создания URL в коде
+router.register('companies', CompanyViewSet, basename='company')
+router.register('students', StudentViewSet, basename='student')
+router.register('resumes', ResumeViewSet, basename='resume')
+router.register('applications', ApplicationViewSet, basename='application')
+
+urlpatterns = router.urls
